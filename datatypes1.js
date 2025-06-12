0295431713731dd67df839988fb002d1a460adcf
Huda Hajira <hudahajira744@gmail.com>
2025-06-11 20:03:57 -0500

    Create datatypes1.js

---
+++ datatypes1.js
@@ -0,0 +1,58 @@
+// Datatypes
+// Primitive datatypes: Number, String and boolean - holds single value
+// composite datatypes: Array, object, function - multiple values
+
+// undefined 
+// NaN - not a number
+
+// String
+var a = "iamastring"
+var a = "i am a string"
+var a = 'iam a string'
+// var a = new String("i am a string"  )
+// console.log(a, typeof a );
+
+// "hi i'am a "joker""
+
+// Escape Characters
+
+// var b = "hi i\'am a \"joker\""
+// console.log(b)
+
+
+// shazia's favourite food is briyani, but anaya's favourite food is Nihari
+// var a= "shazia\'s favourite food is briyani,\  but anaya\'s favourite food is Nihari"
+// console.log(a)
+
+// var  a = "favourite food";
+// var b = "shazia";
+// var c = "anaya";
+// console.log(`${b}'s ${a} is briyani, but ${c}'s ${a} is Nihari`);
+
+// Indexing
+var d= 'smart'
+
+// string = 's'.'m'.'a'.'r'.'t'.
+// index= 0 1 2 3 4 
+// negative index: -5. -4.-3,-2,-1
+
+// console.log(d[0]);
+// console.log(d[3]);
+// console.log(d[-1]);
+
+// Slice
+// console.log(d.slice(2));
+// console.log(d.slice(1,3));
+
+// console.log(d.slice(-2));
+// console.log(d.slice(-2,-4));
+
+
+var a ='smart'
+// console.log(a.indexOf('s'))
+console .log(a.indexOf('2'))
+// why -1? because JS uses -1 as a special signal that "what you searched for is not a string"
+
+// console.log(a.length)
+
+
