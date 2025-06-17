041ed8ec46fb688f774a47f3f5fa7e9aa6423636
Huda Hajira <hudahajira744@gmail.com>
2025-06-16 20:10:57 -0500

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -62,194 +62,39 @@ console.log(a.indexOf('3'))
 console.log(a.length)
 
 
-// comparision of var, let , const
-// Hoisting
-
-// var
-
-// a = 3
-
-// var a;
-// var a=3;
-// a=4;
-
-// var a=3;
-// var a=4;
-// console.log(a)
-
-// var a=3
-// console.log(a)
-// {
-//     console.log(a)
-// }
+// u=[1,2,3,5,4,9,8,6]
 
 
-// var a=3
-// console.log(a)
-// {    
-//     var a=4
-//     console.log(a)
-// }
 
-// var a=3
-// console.log(a)
-// {    
-//     var a=4
-//     console.log(a)
-//     a = 5
-// }
-// console.log(a)
+// // u.pop()
+// u.splice(3,1)
+// console.log(u)
 
-// By default initialization is var
-// var is global
-// Redeclaring is possible
-// Redefining is possible
-
-// let
-
-// let a;
-// a=6
+// let fruits =['apple', 'banana', 'cherry', 'date']
+// fruits.splice(0,1, 'Grape fruit');
+// console.log(fruits)
 
 
-// let a=7;
-// a=8;
-// console.log(a)
+// u.push(7)
+// u.push(6)
+// u.push(7,7,8)
+// u.push([8,9,10])
+
+a=[1,2,3,4]
+console.log(a)
+// a.unshift(0)
+a.shift();
 
 
-// let a;
-// let a=8;
-// cannot be redeclared as let
-// console.log(a)
 
 
-// let a=7;
-// let a=8;
-// error because cannot be redeclared as let
-// console.log(a)
-
-// let a=3
-// console.log(a)
-// {
-//     console.log(a)
-// }
-
-// let a=3
-// console.log(a)
-// { 
-//     let a=4
-//     console.log(a)
-// }
-// console.log(a)
 
 
-// let a=3
-// console.log(a)
-// { 
-//     a=4
-//     console.log(a)
-// }
-// a=6
-// console.log(a)
-
-// let is global
-// Redeclaring is not possible
-// Redefining is possible
-
-// let a=3
-// console.log(a)
-// { 
-//     let a=4
-//     console.log(a)
-// }
-// a=6
-// console.log(a)
 
 
-// let a=3
-// console.log(a)
-// { 
-//     let a=4
-//     console.log(a)
-// }
-// let a=6
-// console.log(a)
 
 
-// const
 
-// const a=3;
-// console.log(a)
-
-// const a;
-// a=4
-
-// const a=3;
-// const a=5;
-
-// var a=4;
-// const a=3;
-
-// a=4;
-// const a=3;
-// console.log(a)
-
-// let a=4;
-// const a=3;
-// console.log(a)
-
-// const a=4
-// console.log(a)
-// {   const a=7
-//     console.log(a)
-// }
-// console.log(a)
-
-// const a=4
-// console.log(a)
-// {   let a=7
-//     console.log(a)
-// }
-// // var a=8;
-// console.log(a)
-
-
-// var a = 8;
-// {
-//     var a=0;
-//     a=90
-//     {
-//         const a=9;
-//         console.log(a)
-//     }
-//     console.log(a)
-// }
-// console.log(a)
-
-
-// let a = 8;
-// {
-//     const a=0;
-//     // a=90
-//     {
-//         const a=9;
-//         console.log(a)
-//     }
-//     console.log(a)
-// }
-// console.log(a)
-
-// var a = 8
-// {
-//     console.log(a)
-//     {
-//         console.log(b)
-//         const b = 9
-//         // console.log(a)
-//     }
-//     console.log(a)
-//     let a =99
-// }
-// console.log(a)
 
 // Arrays
 // let a=['a','b','c','d']
